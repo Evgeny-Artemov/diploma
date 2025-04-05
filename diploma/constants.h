a2274627e4426037c1e63c0d0a8ea6f8ae7260d8
Evgeny-Artemov <artemovevgeny03@gmail.com>
2025-04-05 14:11:59 +0000

    true ODE

--- diploma/constants.h
+++ diploma/constants.h
@@ -8,16 +8,20 @@ namespace Constants {
     // 1 астрономическая единица (расстояние от Земли до Солнца)
     double UnitR = 149598261;  // (149 600 000 км)
     
-    double UnitT = 84000; // c 1 день
+    // double UnitT = 84000; // c 1 день
 
     // Обезразмеренный гравитационный параметр
     // double UnitNu = UnitR * UnitR * UnitR / (UnitT * UnitT); 
-    double UnitNu = UnitR * UnitR * UnitR / (UnitT * UnitT);
+    // double UnitNu = UnitR * UnitR * UnitR / (UnitT * UnitT);
+    double UnitNu = 132712440018;
     // 
-    double UnitV = UnitR / UnitT ; // 
+    // double UnitV = UnitR / UnitT ; // 
+    double UnitV = std::sqrt(UnitNu / UnitR) ; // 
+    double UnitT = UnitR / UnitV;
 
-    double UnitAcc =  UnitR / (UnitT * UnitT); // км/с2
+    // double UnitAcc =  UnitR / (UnitT * UnitT); // км/с2
     
+    double UnitAcc =  UnitNu / (UnitR * UnitR); // км/с2
     // Взята масса топлива КА
     double UnitMass = 1000; // кг
 
--- diploma/integrator/main.cpp
+++ diploma/integrator/main.cpp
@@ -14,34 +14,6 @@ using namespace boost::numeric::odeint;
 // Определяем тип для вектора состояния системы
 typedef std::vector<double> state_type;
 
-// Функция-наблюдатель для вывода результатов во время интегрирования
-void observer(const state_type &x, const double t) {
-    cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT  << ", Положение = (" 
-    << MakeDimensional(x[0], Constants::UnitR) << ", "
-    << MakeDimensional(x[1], Constants::UnitR)  << ", "
-    << MakeDimensional(x[2],  Constants::UnitR) << ")" << endl;
-
-    cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Скорость = ("
-    << MakeDimensional(x[3], Constants::UnitV) << ", "
-    << MakeDimensional(x[4], Constants::UnitV) << ", "
-    << MakeDimensional(x[5], Constants::UnitV) << ")" << endl;
-
-    cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Масса = " << MakeDimensional(x[6], Constants::UnitMass) << endl;
-
-    cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Cопряженные к координатам = " << x[7] << ", " << x[8] << ", " << x[9] << ")" << endl;
-    cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Cопряженные к скорости = "<< x[10] << ", " << x[11] << ", " << x[12] << ")" << endl;
-    cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Cопряженная к массе = " << x[13] << endl;
-}
-
-
-double random_double() {
-    static std::random_device rd;  
-    static std::mt19937 gen(rd()); 
-    static std::uniform_real_distribution<double> dis(-1.0, 1.0);
-    return dis(gen);
-}
-
-
 namespace ZeroCondeitions {
     double X = -1.321562505626410E+08;
     double Y = 6.658171852356435E+07;
@@ -55,28 +27,24 @@ namespace ZeroCondeitions {
     double Weffective = 70; // км/с
 }
 
-double CalcH(double lambdaRI, double lambdaVI, double lambdaM, double lambdaRINorm, double currentMass, double rI, double r_magnitude,  double vI, double delta, double PNorm, double WNorm, double NuNorm) {
-    return -(lambdaVI * NuNorm * rI / (r_magnitude * r_magnitude * r_magnitude)) + delta * PNorm * lambdaRINorm * lambdaVI / currentMass + lambdaRI * vI + lambdaM * delta * PNorm / WNorm  ;
-
-}
 int main() {
     // Инициализируем вектор состояния с 14 элементами
     state_type x(14);
-    
+
     // Устанавливаем безразмерные начальные условия
     // Положение в ЕД (Единицах Длины)
     x[0] = MakeNonDimensional(ZeroCondeitions::X, Constants::UnitR, "Координата Х ");         // X
     x[1] = MakeNonDimensional(ZeroCondeitions::Y, Constants::UnitR, "Координата Y ");          // Y
     x[2] = MakeNonDimensional(ZeroCondeitions::Z, Constants::UnitR, "Координата Z ");      // Z
-    
+
     // Скорость в ЕД/ЕВ (Единицах Длины/Единицах Времени)
     x[3] = MakeNonDimensional(ZeroCondeitions::VX, Constants::UnitV, "Скорость Vx ");      // VX
     x[4] = MakeNonDimensional(ZeroCondeitions::VY, Constants::UnitV, "Скорость Vy ");     // VY
     x[5] = MakeNonDimensional(ZeroCondeitions::VZ, Constants::UnitV, "Скорость Vy ");       // VZ
-    
+
     // Начальная масса в ЕМ (Единицах Массы)
     x[6] = MakeNonDimensional(1000, Constants::UnitMass,"Масса"); //начальная масса топлива
-    
+
     // Инициализируем сопряженные переменные
     for (int i = 7; i < 14; i++) {
         x[i] = 0.1;
@@ -84,105 +52,143 @@ int main() {
 
     auto WNorm = MakeNonDimensional(ZeroCondeitions::Weffective, Constants::UnitV, "Эфективная скорость исчечения газов ");
     auto PNorm = MakeNonDimensional(ZeroCondeitions::P, Constants::UnitP, "Тяга ");
-    auto NuNorm = MakeNonDimensional(Constants::Nu, Constants::UnitNu, "К ");
+    auto NuNorm = MakeNonDimensional(Constants::Nu, Constants::UnitNu, "Поостоянная солнца ");
     // Определяем тип интегратора (Рунге-Кутта-Фельберг 7(8))
     typedef runge_kutta_fehlberg78<state_type> stepper_type;
 
+    std::cout << "Время " << Constants::UnitT << std::endl;
     std::cout << "NuNorm" << NuNorm << std::endl;
-    // std::cout << "NuNorm2" << Constants::UnitNu << std::endl;
     // Определяем систему дифференциальных уравнений
-    auto system = [&](const state_type &x, state_type &dxdt, double t) {
-        double delta = 0.0;  // Изначально отключено (нет тяги)
-  
-        double curentX = x[0];
-        double curentY = x[1];
-        double curentZ = x[2];
+    double delta = 0.0;
 
-        double curentVx = x[3];
-        double curentVy = x[4];
-        double curentVz = x[5];
+    auto observer = [&delta, &WNorm](const state_type &x, const double t) {
 
         double currentMass = x[6];
-        if (currentMass < 1e-10) currentMass = 1e-10;  // Избегаем деления на ноль
-
-        double lambdaX = x[7];
-        double lambdaY = x[8];
-        double lambdaZ = x[9];
 
         double lambdaVx = x[10];
         double lambdaVy = x[11];
         double lambdaVz = x[12];
-
         double lambdaM = x[13];
-        // Вычисляем направление тяги (единичный вектор)
+
         double lambdaVNorm = VectorLength(lambdaVx, lambdaVy, lambdaVz);
-        
-        // Единичный вектор направления тяги (если lambda_v_norm не ноль)
-        // auto curent_delta = 
         double p0_x = 0.0, p0_y = 0.0, p0_z = 0.0;
         if (lambdaVNorm > 1e-10) {
-            p0_x = lambdaX / lambdaVNorm;
+            p0_x = lambdaVx / lambdaVNorm;
             p0_y = lambdaVy / lambdaVNorm;
             p0_z = lambdaVz / lambdaVNorm;
-            
-            // Применяем принцип максимума: если функция переключения < 0, тяга включена 
-            double switching_function = lambdaVNorm / currentMass + lambdaM / WNorm;
+
+            double switching_function = lambdaVNorm / currentMass - lambdaM / WNorm;
             delta = (switching_function > 0) ? 1.0 : 0.0;
         }
-        // Вычисляем величину радиус-вектора
-        double r = VectorLength(curentX, curentY, curentY);
-        double rCubed = r * r * r;
 
-        // Избегаем деления на ноль
-        if (rCubed < 1e-10) rCubed = 1e-10;
-        
+        cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT  << ", Положение = ("
+        << MakeDimensional(x[0], Constants::UnitR) << ", "
+        << MakeDimensional(x[1], Constants::UnitR)  << ", "
+        << MakeDimensional(x[2],  Constants::UnitR) << ")" << endl;
+
+        cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Скорость = ("
+        << MakeDimensional(x[3], Constants::UnitV) << ", "
+        << MakeDimensional(x[4], Constants::UnitV) << ", "
+        << MakeDimensional(x[5], Constants::UnitV) << ")" << endl;
+
+        cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Масса = " << MakeDimensional(x[6], Constants::UnitMass) << endl;
+
+        cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Cопряженные к координатам = " << x[7] << ", " << x[8] << ", " << x[9] << ")" << endl;
+        cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Cопряженные к скорости = "<< x[10] << ", " << x[11] << ", " << x[12] << ")" << endl;
+        cout << "t = " << MakeDimensional(t, Constants::UnitT) / Constants::UnitT << ", Cопряженная к массе = " << x[13] << endl;
+    };
+
+    auto system = [&WNorm, &PNorm, &NuNorm, &delta](const state_type &x, state_type &dxdt, double t) {
+
+        const double curentX = x[0];
+        const double curentY = x[1];
+        const double curentZ = x[2];
+
+        const double curentVx = x[3];
+        const double curentVy = x[4];
+        const double curentVz = x[5];
+
+        const double currentMass = x[6];
+
+        const double lambdaX = x[7];
+        const double lambdaY = x[8];
+        const double lambdaZ = x[9];
+
+        const double lambdaVx = x[10];
+        const double lambdaVy = x[11];
+        const double lambdaVz = x[12];
+
+        const double lambdaM = x[13];
+
+        // Вычисляем направление тяги (единичный вектор)
+        const double lambdaVNorm = VectorLength(lambdaVx, lambdaVy, lambdaVz);
+
+        // Единичный вектор направления тяги (если lambda_v_norm не ноль)
+
+        double Lambda0_x = 0.0, Lambda0_y = 0.0, Lambda0_z = 0.0;
+        if (lambdaVNorm > 1e-10) {
+            Lambda0_x = lambdaVx / lambdaVNorm;
+            Lambda0_y = lambdaVy / lambdaVNorm;
+            Lambda0_z = lambdaVz / lambdaVNorm;
+        }
+
+        cout << "Integrator delta : " << delta << endl;
+
+        // Вычисляем величину радиус-вектора
+        const double r = VectorLength(curentX, curentY, curentZ);
+        const double r3 = r * r * r;
+        const double r5 = r * r * r * r * r;
+
+        // std::cout << "H1 " << VectorLength(Hx, Hy, Hz) << std::endl;
+
+        double dVx_dt = -NuNorm * curentX / r3 + delta * PNorm * Lambda0_x / currentMass;
+        double dVy_dt = -NuNorm * curentY / r3 + delta * PNorm * Lambda0_y / currentMass;
+        double dVz_dt = -NuNorm * curentZ / r3 + delta * PNorm * Lambda0_z / currentMass;
+
+        double H = lambdaVx * dVx_dt + lambdaVy * dVy_dt + lambdaVz * dVz_dt + lambdaX * curentVx + lambdaY * curentVy + lambdaZ * curentVz + lambdaM * (delta * PNorm) / WNorm;
+        std::cout << "H2 " << H << std::endl;
+
         // Дифференциальные уравнения для переменных состояния
-        
+
         // dr/dt = V
-        dxdt[0] = x[3];  // dx/dt = VX
-        dxdt[1] = x[4];  // dy/dt = VY
-        dxdt[2] = x[5];  // dz/dt = VZ
+        dxdt[0] = curentVx;  // dx/dt = VX
+        dxdt[1] = curentVy;  // dy/dt = VY
+        dxdt[2] = curentVz;  // dz/dt = VZ
 
         // dV/dt = -mu * r/|r|^3 + delta * P * p0 / current_mass
-        dxdt[3] = -NuNorm * x[0] / rCubed + delta * PNorm * p0_x / x[6];
-        dxdt[4] = -NuNorm * x[1] / rCubed + delta * PNorm * p0_y / x[6];
-        dxdt[5] = -NuNorm * x[2] / rCubed + delta * PNorm * p0_z / x[6];
-        
+        dxdt[3] = dVx_dt;
+        dxdt[4] = dVy_dt;
+        dxdt[5] = dVz_dt;
+
         // dm/dt = - delta * W / P (расход топлива)
-        dxdt[6] = -delta * PNorm / WNorm;
-        
+        dxdt[6] = - delta * PNorm / WNorm;
+
         // Уравнения для сопряженных переменных (выведены из принципа максимума Понтрягина)
-        
+
         // d(lambda_r)/dt
-        dxdt[7] = (NuNorm * x[10] / rCubed) - (3 * x[0] * x[0] * NuNorm * x[10]) / (rCubed * r * r);
-        dxdt[8] = (NuNorm* x[11] / rCubed) - (3 * x[1] * x[1] * NuNorm * x[11]) / (rCubed * r * r);
-        dxdt[9] = (NuNorm * x[12] / rCubed) - (3 * x[2] * x[2] * NuNorm * x[12]) / (rCubed * r * r);
+        dxdt[7] = lambdaVx * NuNorm / r3 - 3 * NuNorm * curentX / r5 * (lambdaVx * curentX + lambdaVy * curentY + lambdaVz * curentZ);
+        dxdt[8] = lambdaVy * NuNorm / r3 - 3 * NuNorm * curentY / r5 * (lambdaVx * curentX + lambdaVy * curentY + lambdaVz * curentZ);
+        dxdt[9] = lambdaVz * NuNorm / r3 - 3 * NuNorm * curentZ / r5 * (lambdaVx * curentX + lambdaVy * curentY + lambdaVz * curentZ);
 
         // d(lambda_V)/dt = -lambda_r
-        dxdt[10] = -x[7];
-        dxdt[11] = -x[8];
-        dxdt[12] = -x[9];
-        
-        dxdt[13] = -delta * PNorm / (x[6] * x[6]) * lambdaVNorm;
+        dxdt[10] = -lambdaX;
+        dxdt[11] = -lambdaY;
+        dxdt[12] = -lambdaZ;
 
-        auto Hx = CalcH(lambdaX, lambdaVx, lambdaM, p0_x, currentMass, curentX, r, curentVx, delta, PNorm, WNorm, NuNorm);
-        auto Hy = CalcH(lambdaY, lambdaVy, lambdaM, p0_y, currentMass, curentY, r, curentVy, delta, PNorm, WNorm, NuNorm);
-        auto Hz = CalcH(lambdaZ, lambdaVz, lambdaM, p0_z, currentMass, curentZ, r, curentVz, delta, PNorm, WNorm, NuNorm);
-
-        std::cout << VectorLength(Hx, Hy, Hz) << std::endl;
+        dxdt[13] = delta * PNorm / (currentMass * currentMass) * lambdaVNorm;
     };
 
     // Создаем контролируемый интегратор с заданными допусками на ошибку
-    auto stepper = make_controlled<stepper_type>(1.0e-10, 1.0e-10);
+    auto stepper = make_controlled<stepper_type>(1.0e-20, 1.0e-20);
 
     // Выполняем адаптивное интегрирование с функцией-наблюдателем
     auto q = integrate_adaptive(
         stepper,
         system,
         x,
-        1.0,    // Начальное время (в ЕВ)
-        365.0,  // Конечное время (в ЕВ) - настроено для безразмерных единиц
-        1.0,    // Начальный шаг (в ЕВ)
+        365.0,    // Начальное время (в ЕВ)
+        3000.0,  // Конечное время (в ЕВ) - настроено для безразмерных единиц
+        0.1,   // Начальный шаг (в ЕВ)
         observer
     );
 
